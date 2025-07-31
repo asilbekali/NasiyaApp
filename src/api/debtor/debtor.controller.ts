19c669d3ecec6f560bec496f758d6a61c514d3f6
asilbekali <asilbekabdugafforov7@gmail.com>
2025-07-31 19:03:18 +0500

    debtor writing....

--- src/api/debtor/debtor.controller.ts
+++ src/api/debtor/debtor.controller.ts
@@ -1,32 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { DebtorService } from './debtor.service';
 import { CreateDebtorDto } from './dto/create-debtor.dto';
 import { UpdateDebtorDto } from './dto/update-debtor.dto';
+import { RolesGuard } from 'src/common/Guards/roles.guard';
+import { RoleDec } from 'src/common/decoratos/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
+import { AuthGuard } from 'src/common/Guards/auth.guard';
 
 @Controller('debtor')
 export class DebtorController {
   constructor(private readonly debtorService: DebtorService) {}
 
+  @RoleDec(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createDebtorDto: CreateDebtorDto) {
     return this.debtorService.create(createDebtorDto);
   }
 
+  @RoleDec(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.debtorService.findAll();
   }
 
+  @RoleDec(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.debtorService.findOne(+id);
   }
 
+  @RoleDec(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDebtorDto: UpdateDebtorDto) {
     return this.debtorService.update(+id, updateDebtorDto);
   }
 
+  @RoleDec(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.debtorService.remove(+id);
--- src/api/debtor/debtor.module.ts
+++ src/api/debtor/debtor.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DebtorService } from './debtor.service';
 import { DebtorController } from './debtor.controller';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [PrismaModule, JwtModule],
   controllers: [DebtorController],
   providers: [DebtorService],
 })
