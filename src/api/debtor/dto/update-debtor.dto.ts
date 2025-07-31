1c31b7f2c144ce1f1243fded97e2f520393db530
asilbekali <asilbekabdugafforov7@gmail.com>
2025-07-31 20:07:35 +0500

    add update debtor deto

--- src/api/debtor/dto/update-debtor.dto.ts
+++ src/api/debtor/dto/update-debtor.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDebtorDto } from './create-debtor.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateDebtorDto extends PartialType(CreateDebtorDto) {}
+export class UpdateDebtorDto extends PartialType(CreateDebtorDto) {
+  @ApiProperty({ example: 'Ali Valiyev', required: false })
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @ApiProperty({ example: '+998901234567', required: false })
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+
+  @ApiProperty({ example: 'Toshkent, Yunusobod tuman', required: false })
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @ApiProperty({ example: 'Doimiy mijoz', required: false })
+  @IsOptional()
+  @IsString()
+  note?: string;
+}
