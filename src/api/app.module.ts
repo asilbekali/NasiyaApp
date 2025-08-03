67d6211445fecddaca53cb0b9d9e2ef155ed801b
asilbekali <asilbekabdugafforov7@gmail.com>
2025-08-03 16:07:55 +0500

    some file name edited

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -5,7 +5,7 @@ import { AdminModule } from './admin/admin.module';
 import { MulterModule } from 'src/infrastructure/lib/multe-r/multer.module';
 import { DebtorModule } from './debtor/debtor.module';
 import { SellerModule } from './seller/seller.module';
-import { FeebackModule } from './feeback/feeback.module';
+import { FeebackModule } from './feedback/feeback.module';
 import { BorrowedProductModule } from './borrowed-product/borrowed-product.module';
 import { PaymentSectionModule } from './payment-section/payment-section.module';
 
